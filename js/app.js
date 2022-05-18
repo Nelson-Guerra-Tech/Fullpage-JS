81dcba47e414b256b2faf30448470ccc588d2440
Nelson Guerra <nelsonguerra.tech@outlook.com>
2022-05-18 16:09:32 -0400

    Added the contact section and styling with bootstrap

--- js/app.js
+++ js/app.js
@@ -5,4 +5,50 @@ new fullpage("#fullpage", {
   autoScrolling: false,
   anchors: ["home", "projects", "projects2", "about", "contact"],
   parallax: false,
+  scrollBar: false,
 });
+
+// targetting elements
+const navigation = document.querySelector(".links");
+
+colorChange = () => {
+  const links = navigation;
+  if (window.document.contains("s2")) {
+    links.style.color = "#fbfbfb";
+  }
+  console.log(links);
+};
+
+colorChange();
+
+// Validating Form
+function validateForm() {
+  var name = document.getElementById("name").value;
+  if (name == "") {
+    document.querySelector(".status").innerHTML = "Name cannot be empty";
+    return false;
+  }
+  var email = document.getElementById("email").value;
+  if (email == "") {
+    document.querySelector(".status").innerHTML = "Email cannot be empty";
+    return false;
+  } else {
+    var re =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(email)) {
+      document.querySelector(".status").innerHTML = "Email format invalid";
+      return false;
+    }
+  }
+  var subject = document.getElementById("subject").value;
+  if (subject == "") {
+    document.querySelector(".status").innerHTML = "Subject cannot be empty";
+    return false;
+  }
+  var message = document.getElementById("message").value;
+  if (message == "") {
+    document.querySelector(".status").innerHTML = "Message cannot be empty";
+    return false;
+  }
+  document.querySelector(".status").innerHTML = "Sending...";
+}
