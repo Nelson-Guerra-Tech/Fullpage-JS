fd6464f4fb6f9787fd6e2147dc671e927d19039f
Nelson Guerra <nelsonguerra.tech@outlook.com>
2022-05-18 14:15:41 -0400

    added about me section with minor styling

--- js/app.js
+++ js/app.js
@@ -1,11 +1,8 @@
 // Fullpage Initialization
 new fullpage("#fullpage", {
   licenseKey: "B04KK-41DM9-I5XY6-O0O08-RQNOO",
-  sectionsColor: ["#fbfbfb", "#0C0E07", "#0C0E07", "#ADD8E6"],
-  autoScrolling: true,
-  anchors: ["home", "projects", "about", "contact"],
-  parallax: true,
-  onLeave: function (origin, destination, direction) {
-    console.log("Leaving section" + origin.index);
-  },
+  sectionsColor: ["#fbfbfb", "#0C0E07", "#0C0E07", "#fbfbfb", "#fbfbfb"],
+  autoScrolling: false,
+  anchors: ["home", "projects", "projects2", "about", "contact"],
+  parallax: false,
 });
