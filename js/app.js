d315e22635d84685ad42ff44c3592ea0f41099b1
Nelson Guerra <nelsonguerra.tech@outlook.com>
2022-05-17 21:44:13 -0400

    added projects section and completed the cards

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Fullpage Initialization
 new fullpage("#fullpage", {
   licenseKey: "B04KK-41DM9-I5XY6-O0O08-RQNOO",
-  sectionsColor: ["#fbfbfb", "#0C0E07", "#C0C0C0", "#ADD8E6"],
+  sectionsColor: ["#fbfbfb", "#0C0E07", "#0C0E07", "#ADD8E6"],
   autoScrolling: true,
   anchors: ["home", "projects", "about", "contact"],
   parallax: true,
