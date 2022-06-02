cebdcedf27e9e9b43dad3e7abef3f3b9de6ba3b9
Nelson Guerra <nelsonguerra.tech@outlook.com>
2022-06-02 15:25:57 -0400

    Changed formatting for project section and contact section

--- js/app.js
+++ js/app.js
@@ -1,41 +1,101 @@
 // Fullpage Initialization
 new fullpage("#fullpage", {
   licenseKey: "B04KK-41DM9-I5XY6-O0O08-RQNOO",
-  sectionsColor: ["#fbfbfb", "#0C0E07", "#0C0E07", "#fbfbfb", "#fbfbfb"],
+  sectionsColor: ["#fbfbfb", "#f1f5f8", "#fbfbfb", "#f1f5f8"],
   autoScrolling: true,
   anchors: ["home", "projects", "projects2", "about", "contact"],
   parallax: false,
   scrollBar: false,
 });
 
-// Validating Form
-function validateForm() {
-  var name = document.getElementById("name").value;
-  if (name == "") {
-    document.querySelector(".status").innerHTML = "Name cannot be empty";
-    return false;
-  }
-  var email = document.getElementById("email").value;
-  if (email == "") {
-    document.querySelector(".status").innerHTML = "Email cannot be empty";
-    return false;
-  } else {
-    var re =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!re.test(email)) {
-      document.querySelector(".status").innerHTML = "Email format invalid";
-      return false;
-    }
-  }
-  var subject = document.getElementById("subject").value;
-  if (subject == "") {
-    document.querySelector(".status").innerHTML = "Subject cannot be empty";
-    return false;
-  }
-  var message = document.getElementById("message").value;
-  if (message == "") {
-    document.querySelector(".status").innerHTML = "Message cannot be empty";
-    return false;
-  }
-  document.querySelector(".status").innerHTML = "Sending...";
+// project cards
+const card = [
+  {
+    id: 1,
+    title: "Project Title",
+    img: "./img/project-img.jpg",
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    tech_icon: {
+      html: "./img/icons8-html-5-96.svg",
+      css: "./img/icons8-css3-96.svg",
+      javascript: "./img/icons8-javascript.svg",
+      bootstrap: "./img/icons8-bootstrap.svg",
+      react: "./img/icons8-react-native.svg",
+    },
+    href: "#",
+  },
+  {
+    id: 2,
+    title: "Project Title",
+    img: "./img/project-img.jpg",
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    tech_icon: {
+      html: "./img/icons8-html-5-96.svg",
+      css: "./img/icons8-css3-96.svg",
+      javascript: "./img/icons8-javascript.svg",
+      bootstrap: "./img/icons8-bootstrap.svg",
+      react: "./img/icons8-react-native.svg",
+    },
+    href: "#",
+  },
+  {
+    id: 3,
+    title: "Project Title",
+    img: "./img/project-img.jpg",
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    tech_icon: {
+      html: "./img/icons8-html-5-96.svg",
+      css: "./img/icons8-css3-96.svg",
+      javascript: "./img/icons8-javascript.svg",
+      bootstrap: "./img/icons8-bootstrap.svg",
+      react: "./img/icons8-react-native.svg",
+    },
+    href: "#",
+  },
+  {
+    id: 4,
+    title: "Project Title",
+    img: "./img/project-img.jpg",
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    tech_icon: {
+      html: "./img/icons8-html-5-96.svg",
+      css: "./img/icons8-css3-96.svg",
+      javascript: "./img/icons8-javascript.svg",
+      bootstrap: "./img/icons8-bootstrap.svg",
+      react: "./img/icons8-react-native.svg",
+    },
+    href: "#",
+  },
+];
+
+const cardsSection = document.querySelector(".cards");
+
+// load items
+window.addEventListener("DOMContentLoaded", function () {
+  displayCards(card);
+});
+
+// function to display the cards
+function displayCards(cards) {
+  let displayCard = cards.map(function (item) {
+    // console.log(item);
+
+    return `<article class="card">
+            <img class="project-img" src="${item.img}" alt="project-img"/>
+            <div class="project-info">
+              <h3>${item.title}</h3>
+              <p>${item.desc}</p>
+              <div class="tech-icons-container">
+                <img class="tech-icon" src="${item.tech_icon.html}" alt"technologies"/>
+                <img class="tech-icon" src="${item.tech_icon.css}" alt"technologies"/>
+                <img class="tech-icon" src="${item.tech_icon.javascript}" alt"technologies"/>
+                <img class="tech-icon rotate" src="${item.tech_icon.react}" alt"technologies"/>
+              </div>
+                <a href="${item.href}">Live Demo</a>
+            </div>
+          </article>`;
+  });
+  displayCard = displayCard.join("");
+
+  cardsSection.innerHTML = displayCard;
 }
